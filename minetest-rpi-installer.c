0e54cea2f6d5ac267e92766797b0b90af4e2db81
spartrekus <spartrekus@gmail.com>
2018-10-14 10:12:53 +0200

    Update minetest-rpi-installer.c

--- minetest-rpi-installer.c
+++ minetest-rpi-installer.c
@@ -11,6 +11,8 @@ int main()
     system( " apt-get update; apt-get install -y blackbox xinit xterm minetest "  );
     printf( " Bring rpi to OpenGL driver (Full) \n" );
     system( " raspi-config" );
+      // once done, install, and run minetest from xterm and then go to settings and remove filter, antialiasing, remove the texture, remove ticks and it runs fast on 800x600
+      // x11 xserver utils and run xrandr -s 800x600 into your .xinitrc 
     return 0;
 }
 
